e5ca87bfa2a849a5297ecef9e1a0164333d0f74a
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2024-09-12 02:35:58 +0500

    users refac

--- app/components/Users.tsx
+++ app/components/Users.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useMemo } from 'react';
 import { styled } from 'styled-components';
 import { Tabs } from './Tabs';
 import { User } from './User';
@@ -9,36 +9,37 @@ const StyledUsersContainer = styled.div`
   padding: 16px;
 `;
 
+const filterUsers = (users: UserPropsType, departmentFilter: string) => {
+  return users.filter(user => 
+    departmentFilter === 'all' || user.department === departmentFilter
+  )
+}
+
 export const Users = ({
   users
 }: {
   users: UserPropsType;
 }) => {
   const [departmentFilter, setDepartmentFilter] = useState('all');
+  const filteredUsers = useMemo(() => filterUsers(users, departmentFilter), [users, departmentFilter]);
 
   return (
     <section>
       <Tabs onDepartmentChange={setDepartmentFilter}/>
       <StyledUsersContainer>
-        {users &&
-          users
-          .filter((user: UserType) => {
-            if(departmentFilter !== 'all'){
-              return user.department === departmentFilter
-            }
-            return users;
-          })
-          .map((user: UserType) => {
-          return (
+        {filteredUsers.length > 0 ? (
+          filteredUsers.map(user => (
             <User
-              key={user.id} 
+              key={user.id}
               firstName={user.firstName}
               lastName={user.lastName}
               userTag={user.userTag}
               department={user.department}
             />
-          )
-        })}
+          ))
+        ) : (
+          <p>No users to display</p>
+        )}
       </StyledUsersContainer>
     </section>
   );
